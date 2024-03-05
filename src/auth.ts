ee076ae8b406579c96f530255f417585e6a1b53a
55555-Jyeon <jyeon380516@gmail.com>
2024-03-05 22:57:28 +0900

    login complete

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,48 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextResponse } from "next/server";
 
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
-} = NextAuth({});
+} = NextAuth({
+  // 직접 만든 로그인과 회원가입 페이지 등록하기 (next가 기본적으로 만들어주는 페이지가 있음)
+  pages: {
+    signIn: "/i/flow/login",
+    newUser: "/i/flow/signup",
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const authResponse = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}}/api/login`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              id: credentials.username,
+              password: credentials.password,
+            }),
+          }
+        );
+        // login fail
+        if (!authResponse.ok) {
+          return null;
+        }
+        // login success
+        const user = await authResponse.json();
+        console.log("user", user);
+        // can check loggedIn user info.
+        return {
+          email: user.id,
+          name: user.nickname,
+          image: user.image,
+          ...user,
+        };
+      },
+    }),
+  ],
+});
